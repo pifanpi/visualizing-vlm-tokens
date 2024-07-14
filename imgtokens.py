6e3e667e1e9e3fa6da0233a849510d3accf587e7
Leo Dirac <leo@groundlight.ai>
2024-07-13 20:47:54 -0700

    Displays the strengths (dot-product magnitudes) in the hovers, with three different similarity metrics.

--- imgtokens.py
+++ imgtokens.py
@@ -11,6 +11,9 @@ from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 import argparse
 import requests
 import torch
+import torch.nn.functional as F
+
+import vecmath
 
 class BailOutWithData(RuntimeError):
     """A glorious hack!  This lets you bail out in the middle of somebody else's code 
@@ -49,20 +52,25 @@ XIAODICT = XiaoDict()
 
 
 class WordList:
-    """Represents all the words in a single token, and provides methods to render them.
+    """Represents all the discovered word-pieces associated with a token.
+    Provides methods to render them.
     """
 
     def __init__(self):
         self.words = []
+        self.strengths = []
 
-    def append(self, word: str):
+    def append(self, word: str, strength: float = 1.0):
+        """Add another word-piece to the list.
+        """
         augmented = XIAODICT.augment(word)
         self.words.append(augmented)
+        self.strengths.append(strength)
 
     def as_html(self) -> str:
         """Renders the words as HTML for a plotly tooltip
         """
-        html = "<br>".join(self.words)
+        html = "<br>".join(f"{w} ({s:.3f})" for w, s in zip(self.words, self.strengths))
         return html
 
     def first(self) -> str:
@@ -79,14 +87,17 @@ class ImagePatchWordTokenizer:
     with plotly.
     """
 
-    def __init__(self, model_name:str = "llava-hf/llava-v1.6-vicuna-7b-hf", use_4bit:bool = True):
-        """model_name is the name of the model to use.  
+    def __init__(self, model_name:str = "llava-hf/llava-v1.6-vicuna-7b-hf", use_4bit:bool = True, similarity:str="dot"):
+        """
+        :arg model_name: is the name of the model to use.  
             Options known to work are:
                 llava-hf/llava-v1.6-mistral-7b-hf
                 llava-hf/llava-v1.6-vicuna-7b-hf
+        :arg similarity: is the similarity metric to use.  Can be "dot", "cosine", or "l2".
         """
         self.model_str = model_name
         self.use_4bit = use_4bit
+        self.similarity = similarity
         self.processor = None 
         self.model = None
         self.img = None
@@ -122,6 +133,32 @@ class ImagePatchWordTokenizer:
             assert isinstance(data, torch.Tensor), f"Expected a tensor, got {type(data)}"
             return data
 
+    def _closest_wordpieces(self, output_vectors: torch.Tensor, k: int = 1) -> tuple[torch.Tensor, torch.Tensor]:
+        """Finds the k closest word-pieces to the output vectors.  Returns the indices and strengths.
+        """
+        # Now we have a bunch of predicted tokens, in vector (embedding) space.
+        # To convert them to token-Ids, we dot them against the embedding matrix.
+        #embedding_matrix = self.model.get_output_embeddings().weight
+        # ^^ returns a strangely empty object which seems like it's all zeros.
+        embedding_matrix = self.model.language_model.model.embed_tokens.weight  # <vocab, dim>
+        # embedding_matrix is shape <vocab, dim>
+        # output_vectors is shape <tok, dim>
+
+        if self.similarity == "dot":
+            similarity_matrix = torch.matmul(output_vectors, embedding_matrix.T)  # <tok, vocab>
+            invert = 1
+        elif self.similarity == "cosine":
+            similarity_matrix = vecmath.all_to_all_cosine_similarity(output_vectors, embedding_matrix)
+            invert = 1
+        elif self.similarity == "l2":
+            similarity_matrix = vecmath.all_to_all_l2_distance(output_vectors, embedding_matrix)
+            invert = -1  # these are distances, so we want to sort for small values
+        else:
+            raise ValueError(f"Unknown similarity metric: {self.similarity}")
+        k_closest = torch.topk(similarity_matrix * invert, k=k, dim=-1).indices  # <tok, k>
+        strengths = torch.gather(similarity_matrix, 1, k_closest)  # <tok, k>
+        return k_closest, strengths
+
     def _vectors_to_words(self, output_vectors: torch.Tensor, num_words: int = 1) -> list[list[WordList]]:
         """Takes the output_vectors and converts them to a list of lists of strings.
         The list of lists represents the geometry of the image tokens.
@@ -133,13 +170,7 @@ class ImagePatchWordTokenizer:
         edge = int(numtok ** 0.5)
         assert edge ** 2 == numtok, f"Expected a perfect square-lengthed number of tokens (e.g. 576=24^2), but got {numtok}"
 
-        # Now we have a bunch of predicted tokens, in vector (embedding) space.
-        # To convert them to token-Ids, we dot them against the embedding matrix.
-        #embedding_matrix = self.model.get_output_embeddings().weight
-        # ^^ returns a strangely empty object which seems like it's all zeros.
-        embedding_matrix = self.model.language_model.model.embed_tokens.weight
-        similarity_matrix = torch.matmul(output_vectors, embedding_matrix.T)
-        k_closest = torch.topk(similarity_matrix, k=num_words, dim=-1).indices
+        k_closest, strengths = self._closest_wordpieces(output_vectors, num_words)
 
         # Now go through it as a square matrix and convert to a list of lists of strings.
         out = []
@@ -149,7 +180,8 @@ class ImagePatchWordTokenizer:
                 idx = i*edge + j
                 entry = WordList()
                 for k in range(num_words):
-                    entry.append(self.processor.tokenizer.decode(k_closest[idx][k]))
+                    wordpiece = self.processor.tokenizer.decode(k_closest[idx][k])
+                    entry.append(wordpiece, strengths[idx][k])
                 line.append(entry)
             out.append(line)
         return out
@@ -217,7 +249,7 @@ class ImagePatchWordTokenizer:
         words = self._vectors_to_words(tokens[0], num_words)
         return words
 
-    def draw_with_plotly(self, words: list[list[str]], size: int = 1500, iframe:bool = True, show_plot:bool = True):
+    def draw_with_plotly(self, words: list[list[str]], size: int = 1500, iframe:bool = True) -> "go.Figure":
         """Renders the image, and overlays a grid with words in it, in iPython notebook using plotly
         :param words: the word list returned by process_img
         :param size: the size of the image to render
@@ -262,8 +294,6 @@ class ImagePatchWordTokenizer:
             textposition="bottom center",
             hoverinfo="text", # prevents pixel coordinates from showing up in hover
         ))
-        if show_plot:
-            fig.show()
         return fig
 
 
